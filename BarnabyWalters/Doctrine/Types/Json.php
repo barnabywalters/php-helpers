507180b8a75b9974ed27677af698aa35eeb9c46e
Barnaby Walters <barnaby@waterpigs.co.uk>
2012-11-12 21:26:19 +0000

    Added Doctrine JSON type

---
+++ BarnabyWalters/Doctrine/Types/Json.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace BarnabyWalters\Doctrine\Types;
+
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
+/**
+ * My custom datatype.
+ */
+class Json extends Type
+{
+    const JSON = 'json'; // modify to match your type name
+
+    public function getSqlDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        // return the SQL used to create your column type. To create a portable column type, use the $platform.
+        return 'LONGTEXT';
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        // This is executed when the value is read from the database. Make your conversions here, optionally using the $platform.
+        return json_decode($value);
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        // This is executed when the value is written to the database. Make your conversions here, optionally using the $platform.
+        return json_encode($value);
+    }
+
+    public function getName()
+    {
+        return self::JSON; // modify to match your constant name
+    }
+}
