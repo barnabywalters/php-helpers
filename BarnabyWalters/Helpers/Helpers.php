b642d8b5afbbd504ec44f7e43a98dfe32fd52e74
Barnaby Walters <barnaby@waterpigs.co.uk>
2012-12-15 12:47:17 +0000

    relativeTimeElement fix for  being a DateTime instance

--- BarnabyWalters/Helpers/Helpers.php
+++ BarnabyWalters/Helpers/Helpers.php
@@ -135,6 +135,8 @@ class Helpers {
             array $attrs = []) {
         if ($comparison == null)
             $comparison = Carbon::now();
+        else
+            $comparison = Carbon::instance($comparison);
         
         /** @var Carbon */
         $dateTime = Carbon::instance($dateTime);
